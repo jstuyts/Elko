89844a838f8f6c62f369a807aba6a6475d1638af
Johan Stuyts <j.stuyts@javathinker.com>
2020-04-01 02:42:33 +0200

    Removed unused class.

--- Running/src/main/java/org/elkoserver/foundation/run/SlowService.java
+++
@@ -1,8 +0,0 @@
-package org.elkoserver.foundation.run;
-
-/**
- * Marker interface to tag worker classes as wanting to be run via the
- * {@link SlowServiceRunner}.
- */
-public interface SlowService {
-}
